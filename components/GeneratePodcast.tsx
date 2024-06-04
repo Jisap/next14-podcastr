8d10726ba6aa9340e1a8e0c14a12885346523936
Jorge Matos <jisapjisap@gmail.com>
2024-06-04 19:26:14 +0200

    actions convex

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -6,7 +6,11 @@ import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { Loader } from 'lucide-react'
-
+import { useToast } from './ui/use-toast'
+import { useAction, useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { v4 as uuidv4 } from 'uuid';
+import { useUploadFiles } from '@xixixao/uploadstuff/react';
 
 const useGeneratePodcast = ({
   setAudio, 
@@ -15,9 +19,65 @@ const useGeneratePodcast = ({
   setAudioStorageId
 }: GeneratePodcastProps) => {
 
+  const [isGenerating, setIsGenerating] = useState(false);
+  const { toast } = useToast();
+
+  const getPodcastAudio = useAction(api.openai.generateAudioAction)
+
+  const getAudioUrl = useMutation(api.podcasts.getUrl);               // Obtiene la url del podcast subido (action podcast)
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl); // Genera una dirección de almacenaje (action file)
+  const { startUpload } = useUploadFiles(generateUploadUrl);          // Sube a esa dirección el file ( action @xixixao/uploadstuff/react)
+
+  const generatePodcast = async () => {
+    setIsGenerating(true);
+    setAudio('');
+
+    if (!voicePrompt) {
+      toast({
+        title: "Please provide a voiceType to generate a podcast",
+      })
+      return setIsGenerating(false);
+    }
+
+    try {
+      const response = await getPodcastAudio({
+        voice: voiceType,
+        input: voicePrompt
+      });
+
+      const blob = new Blob([response], { type: 'audio/mpeg' });        // Formato blob
+      const fileName = `podcast-${uuidv4()}.mp3`;                       // nombre del podcast según uuid  
+      const file = new File([blob], fileName, { type: 'audio/mpeg' });  // blob to File con el nombre uuid
+
+      const uploaded = await startUpload([file]);                       // Sube a convex el file del audio del podcast
+      const storageId = (uploaded[0].response as any).storageId;        // Obtenemos de dicho file subido su Id en convex
+
+      setAudioStorageId(storageId);                                     // Establecemos el estado para audioStorageId   
+
+      const audioUrl = await getAudioUrl({ storageId });                // Obtenemos el url del audio subido a convex
+      setAudio(audioUrl!);                                              // Establece estado para Audio con dicha url  
+      setIsGenerating(false);
+      toast({
+        title: "Podcast generated successfully",
+      })
+
+    } catch (error) {
+      console.log('Error generating podcast', error)
+      toast({
+        title: "Error creating a podcast",
+        variant: 'destructive',
+      })
+      setIsGenerating(false);
+    }
+
+  }
+
+
+
   return {
-    isGenerating: false,
-    generatePodcast: () => {}
+    isGenerating,
+    generatePodcast
   }
 }
 
